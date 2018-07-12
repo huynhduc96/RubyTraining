06e4ca006b56490bd81858e15f97c2b7f92f1b5e
huynhduc96 <huynhduc96@gmail.com>
2018-07-12 08:59:23 +0700

    chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by id: params[:id]
-    if @user.nil?
-      redirect_to "/"
-    end
+    redirect_to root_url unless @user
   end
 
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "users.new.flash"
       redirect_to @user
     else
