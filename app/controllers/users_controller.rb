cc4a221985c25b3df9de0d272e6e9af16f495b3d
huynhduc96 <huynhduc96@gmail.com>
2018-07-13 10:44:30 +0700

    chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :find_user, only: [:edit, :update, :show, :destroy]
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find_by id: params[:id]
     redirect_to root_url unless @user
+  end
+
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.users.per_page
   end
 
   def create
@@ -19,10 +27,48 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "users.new.update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    flash[:success] = t "users.edit.delete"
+    redirect_to users_url
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by id: params[:id]
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "users.new.flash_log_in"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
+User.create!(name:  "Duclh",
+             email: "ducro09@gmail.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             admin: true)
 
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "12345678"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
