5edb4c28e7a19c3d1fc1c17de8973c18f41edd7b
huynhduc96 <huynhduc96@gmail.com>
2018-07-17 12:00:53 +0700

    chapter 13_1

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ User.create!(name:  "Duclh",
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "ducro-#{n+1}@gmail.com"
   password = "12345678"
   User.create!(name:  name,
                email: email,
@@ -16,4 +16,9 @@ User.create!(name:  "Duclh",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
